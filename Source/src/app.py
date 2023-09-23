22ca79bce40ea49a6991e903a47a1c3c719b9179
ThePowerO <136516815+ThePowerO@users.noreply.github.com>
2023-09-23 19:49:32 -0300

    Update app.py

--- Source/src/app.py
+++ Source/src/app.py
@@ -32,10 +32,38 @@ def CreateAccount():
 
     if user_exists:
         return jsonify({'error': 'User already exists'}), 409
-
+    
     hashed_password = bcrypt.generate_password_hash(password)
     new_user = User(email=email, password=hashed_password)
     db.session.add(new_user)
     db.session.commit()
 
+    session["user_id"] = new_user.id
 
+    return jsonify({
+        "id": new_user.id,
+        "email": new_user.email
+    })
+
+@app.route('/LogIn', methods=["POST"])
+def LogIn():
+    email = request.json["email"]
+    password = request.json["password"]
+
+    user = User.query.filter_by(email=email).first()
+
+    if user is None:
+        return jsonify({'error': 'Unauthorized Access'}), 401
+    
+    if not bcrypt.check_password_hash(user.password, password):
+        return jsonify({"error": "Unauthorized"}), 401
+    
+    session["user_id"] = user.id
+
+    return jsonify({
+        "id": user.id,
+        "email": user.email
+    })
+
+if __name__ == "__main__":
+    app.run(debug=True)
