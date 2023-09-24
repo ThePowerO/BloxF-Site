ae39b07bf431e53cff043db4dcad1df93720161f
ThePowerO <136516815+ThePowerO@users.noreply.github.com>
2023-09-24 17:59:59 -0300

    Update Dashboard.js

--- Source/src/pages/Dashboard.js
+++ Source/src/pages/Dashboard.js
@@ -25,7 +25,7 @@ export function Dashboard() {
                         <i className="bi bi-translate"></i>
                     </Link>
                 </div>
-
+            </section>
         
 
         
