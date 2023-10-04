fd006afe72968dfa79253c948ce3fc89afdc37d7
ThePowerO <136516815+ThePowerO@users.noreply.github.com>
2023-10-03 23:38:58 -0300

    Update app.py

--- Source/src/app.py
+++ Source/src/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, request, session
+ from flask import Flask, jsonify, request, session
 from flask_bcrypt import Bcrypt
 from flask_cors import CORS, cross_origin
 from models import db, User
