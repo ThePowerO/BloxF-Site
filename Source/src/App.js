907ccaffde5cea467125fc55042dd8d3a086f27f
ThePowerO <136516815+ThePowerO@users.noreply.github.com>
2024-01-07 23:59:24 -0300

    Update App.js

--- Source/src/App.js
+++ Source/src/App.js
@@ -2,7 +2,7 @@ import { Route, Routes } from "react-router-dom"
 import { ComboCreate } from "./pages/ComboCreate" 
 import { Dashboard } from "./pages/Dashboard"
 import { CreateAccount } from "./pages/CreateAccount" 
-import { LogIn } from "./pages/LogIn"
+import { LogIn } from "./pages/LogIn" 
 import MainPage from "./pages/MainPage"
 
 function App() {
